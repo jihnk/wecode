62acc36553c1254fe65ac4b425b0025c8f0d0ee5
jihnk <jhyunk218@gmail.com>
2021-09-03 10:45:36 +0900

    Add: test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+function getEvenNum () {
+        let result = [];
+        for (let i = 1; i <= 50;i++) {
+                if((i / 2) === 0) {
+                        result+=i
+                }
+        return result;
+}
+~ 
